e1877e14de6e55c2c1b3e73bc95b22d6e406612f
lkon0010 <lkon0010@uni.sydney.edu.au>
2022-04-11 22:12:37 +0800

    Add Test script and Test Config

---
+++ configs/config_test_w_sgm.py
@@ -0,0 +1,19 @@
+
+testset_root = 'datasets/test_2k_540p'
+test_flow_root = 'datasets/test_2k_pre_calc_sgm_flows'
+test_annotation_root = 'datasets/test_2k_annotations'
+
+test_size = (960, 540)
+test_crop_size = (960, 540)
+
+mean = [0., 0., 0.]
+std  = [1, 1, 1]
+
+inter_frames = 1
+
+model = 'AnimeInterp'
+pwc_path = None
+
+checkpoint = 'checkpoints/animeinterp+gma.pth'
+
+store_path = 'outputs/atd-12k'
