4fc93bc9e290683b87db11633fb70daeccd38a52
lkon0010 <lkon0010@uni.sydney.edu.au>
2022-04-11 22:09:24 +0800

    Add Utils

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+# from utils import config, correlation
+from utils import config # Modified according to https://github.com/lisiyao21/AnimeInterp/issues/8#issuecomment-844358741
+
+__all__ = ['config', 'correlation', 'EMA']
