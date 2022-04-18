a6f98116e34556d0e2945ebf49615fa4aa1ee0be
lkon0010 <lkon0010@uni.sydney.edu.au>
2022-04-18 17:08:39 +0800

    Update gen_540p_atd12k.py

--- scripts/gen_540p_atd12k.py
+++ scripts/gen_540p_atd12k.py
@@ -9,7 +9,7 @@ folders = os.listdir(input_dir)
 
 os.mkdir(output_dir)
 
-for cur in folders:
+for i, cur in enumerate(folders):
     files = os.listdir(input_dir + cur)
     os.mkdir(output_dir + cur)
     for f in files:
@@ -17,3 +17,4 @@ for cur in folders:
         img = cv2.imread(input_dir + cur + "/" + f, cv2.IMREAD_UNCHANGED)
         resized = cv2.resize(img, resize_dim, interpolation=cv2.INTER_AREA)
         cv2.imwrite(output_dir + cur + "/" + fname + ".jpg", resized)
+    print("Progress: {:.2f}%".format((i + 1) / len(folders)))
