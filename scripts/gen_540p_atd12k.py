affcf66706bea32baf804f80454a181abc87c1a6
lkon0010 <lkon0010@uni.sydney.edu.au>
2022-04-18 16:05:16 +0800

    Create gen_540p_atd12k.py
    Downsampling script for atd-12k training sets

---
+++ scripts/gen_540p_atd12k.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+
+input_dir = "datasets/train_10k/"
+output_dir = "datasets/train_10k_540p/"
+resize_dim = (960, 540)
+
+folders = os.listdir(input_dir)
+
+os.mkdir(output_dir)
+
+for cur in folders:
+    files = os.listdir(input_dir + cur)
+    os.mkdir(output_dir + cur)
+    for f in files:
+        fname, _ = f.split(".")
+        img = cv2.imread(input_dir + cur + "/" + f, cv2.IMREAD_UNCHANGED)
+        resized = cv2.resize(img, resize_dim, interpolation=cv2.INTER_AREA)
+        cv2.imwrite(output_dir + cur + "/" + fname + ".jpg", resized)
