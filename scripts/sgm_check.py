58c072853c7a1609fbe78fa986f721527863911b
lkon0010 <lkon0010@uni.sydney.edu.au>
2022-05-11 19:40:00 +0800

    Update sgm_check.py

--- scripts/sgm_check.py
+++ scripts/sgm_check.py
@@ -11,7 +11,13 @@ FILE_LIST = [
     "matching_color_1to3.jpg",
     "matching_color_3to1.jpg",
 ]
-sgm_folder = "../datasets/train_10k_pre_calc_sgm_flows"
+sgm_folder = "../datasets/train_10k_pre_calc_sgm_flows/"
 
 folders = os.listdir(sgm_folder)
 assert len(folders) == 10000, "The total number of datasets is not 10k."
+
+for folder in folders:
+    ls = os.listdir(sgm_folder + folder)
+    if FILE_LIST == ls:
+        print(f'{folder} contains missing files.')
+        continue
