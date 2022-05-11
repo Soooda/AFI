6c73e3551a07f00b5f57c939e48ee91d99a40129
lkon0010 <lkon0010@uni.sydney.edu.au>
2022-05-11 19:46:25 +0800

    Update sgm_check.py

--- scripts/sgm_check.py
+++ scripts/sgm_check.py
@@ -2,6 +2,7 @@
 Check if each triplet contains valid sgm flows data
 '''
 import os
+import numpy as np
 
 FILE_LIST = [
     "guide_flo13.jpg",
@@ -16,8 +17,18 @@ sgm_folder = "../datasets/train_10k_pre_calc_sgm_flows/"
 folders = os.listdir(sgm_folder)
 assert len(folders) == 10000, "The total number of datasets is not 10k."
 
+count = 0
 for folder in folders:
+    count += 1
+    print("\rChecking {} / 10000".format(count), end="", flush="")
     ls = os.listdir(sgm_folder + folder)
     if FILE_LIST == ls:
         print(f'{folder} contains missing files.')
+        continue
+
+    try:
+        np.load(sgm_folder + folder + "/guide_flo13.npy")
+        np.load(sgm_folder + folder + "/guide_flo31.npy")
+    except ValueError:
+        print(f'{folder} contains Pickle error.')
         continue
